b6c59bd1dd1a3fa72747e634d8dc29e9d161ed30
Staheos <staslewy@gmail.com>
2024-04-29 22:05:43 +0200

    Changed UI to modern-like

--- adrilight/View/SettingsWindow.xaml.cs
+++ adrilight/View/SettingsWindow.xaml.cs
@@ -40,5 +40,27 @@ namespace adrilight.ui
                 settingsViewModel.IsSettingsWindowOpen = false;
             }
         }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void MinimalizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void DockPanel_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
+        }
     }
 }
